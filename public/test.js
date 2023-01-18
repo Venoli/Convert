8f8d6865b61f2712729deae43c3b31cc67903ee7
venoli <venolishehara@gmail.com>
2023-01-18 15:26:41 +0530

    firebase deploy

--- public/test.js
+++ public/test.js
@@ -1,11 +1,15 @@
- function loadHTML() {
+/**
+ * @author Venoli Gamage <venolishehaara@gmail.com>
+ */
+
+function loadHTML() {
     fetch(URL.createObjectURL(document.getElementById("html_file").files[0]))
         .then(response=> response.text())
         .then(text => convert(text));
 }
 
+
 async  function convert(text){
-    setUpImages();
     document.getElementById('homePage').innerHTML = text
 
     var images = document.querySelectorAll("img")
@@ -16,21 +20,19 @@ async  function convert(text){
     for (const file of imageFiles) {
         imgHash["https://convertbase64.web.app/"+file.webkitRelativePath] = file;
        // imgHash["http://localhost:63342/Convert/untitled/public/"+file.webkitRelativePath] = file;
-
         console.log("https://convertbase64.web.app/"+file.webkitRelativePath)
     }
 
     for (let contentChild of images){
-
         await asycronouseProcess();
 
          async function asycronouseProcess() {
             var reader = new FileReader();
-             console.log("Image.src paths from original html" + contentChild.src)
-            console.log(imgHash[contentChild.src])
-                await reader.readAsDataURL(imgHash[contentChild.src])
+            console.log("Image.src paths from original html" + contentChild.src)
+             console.log(imgHash[contentChild.src])
+             await reader.readAsDataURL(imgHash[contentChild.src])
 
-                var baseString;
+             var baseString;
                 reader.onloadend = await function () {
                     baseString = reader.result;
                     doneCount++
@@ -38,13 +40,10 @@ async  function convert(text){
                     contentChild.src = baseString
                     reader.abort()
                 };
-
-
          }
-
-
     }
 }
+
 
 function save(){
     text = document.getElementById('homePage').innerHTML
@@ -57,8 +56,6 @@ function save(){
         document.body.appendChild(element);
 
         element.click();
-
         document.body.removeChild(element);
-
 }
 
